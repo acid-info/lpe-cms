a30aac1264734cb5d9205b2dd3d4166d83ff3eca
jinhojang6 <jinho5051@gmail.com>
2026-02-05 01:13:54 +0900

    feat: add dynamic blocks for rich text, code block, and interactive embed components chore: update .env.example with database configuration options

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,51 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface BlocksCodeBlock extends Schema.Component {
+  collectionName: 'components_blocks_code_blocks';
+  info: {
+    displayName: 'Code Block';
+    description: '';
+  };
+  attributes: {
+    language: Attribute.String;
+    code: Attribute.Text;
+  };
+}
+
+export interface BlocksInteractiveEmbed extends Schema.Component {
+  collectionName: 'components_blocks_interactive_embeds';
+  info: {
+    displayName: 'Interactive Embed';
+    description: '';
+  };
+  attributes: {
+    title: Attribute.String;
+    full_html: Attribute.Text;
+    html: Attribute.Text;
+    css: Attribute.Text;
+    js: Attribute.Text;
+    height: Attribute.Integer;
+  };
+}
+
+export interface BlocksRichText extends Schema.Component {
+  collectionName: 'components_blocks_rich_texts';
+  info: {
+    displayName: 'Rich Text';
+    description: '';
+  };
+  attributes: {
+    body: Attribute.RichText &
+      Attribute.CustomField<
+        'plugin::ckeditor.CKEditor',
+        {
+          output: 'HTML';
+          preset: 'standard';
+        }
+      >;
+  };
+}
+
 export interface CatChannel extends Schema.Component {
   collectionName: 'components_cat_channels';
   info: {
@@ -17,6 +63,9 @@ export interface CatChannel extends Schema.Component {
 declare module '@strapi/types' {
   export module Shared {
     export interface Components {
+      'blocks.code-block': BlocksCodeBlock;
+      'blocks.interactive-embed': BlocksInteractiveEmbed;
+      'blocks.rich-text': BlocksRichText;
       'cat.channel': CatChannel;
     }
   }
